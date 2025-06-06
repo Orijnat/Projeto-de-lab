ed8b57e0f89b3ce5db5a51315a72d4bc5addfbeb
Orijnat <cledivanleao@gmail.com>
2025-06-06 20:16:17 -0300

    adicionados comentarios na funçoes da aplicaçao

--- codigos/main .c
+++ codigos/main .c
@@ -3,50 +3,60 @@
 #include <stdlib.h>
 #include <string.h>
 #define QNT_CLIENTES
-    int esc,clientes_listados, clientes_cadastrados, produtos_cadastrados;
+    int escolha,clientes_listados, clientes_cadastrados, produtos_cadastrados;
     char clientes[10][50];
     char produtos [10][40];
-    
-    
+
+
+// funçao responsavel por adicionar um cliente no sistema
     void add_cliente(){
         printf("digite o nome do cliente: ");
         scanf("%s", clientes[clientes_cadastrados]);
         clientes_cadastrados++;
         printf("cliente adicionado");
     } 
-    
+
+// funçao responsavel por adicionar produtos ao sistema
     void add_produto(){
         printf("digite o produto que sera adicionado: ");
         scanf("%s", produtos[produtos_cadastrados]);
         produtos_cadastrados++;
         printf("produto cadastrado");
     }
+
+// funçao responsavel por realizar uma venda e gerar uma nota fiscal com os dados utilizados na venda
     void fazer_venda(){
         
     }
-    
+
+// funçao responsavel por listar os clientes ja cadastrados no sistema
    void listar_clientes() {
     for (int i = 0; i < clientes_cadastrados; i++) {
         printf("Listando cliente: %s\n", clientes[i]);
     }
 }
 
-    
+// funçao responsavel por listar os produtos ja cadastrados no sistema
     void listar_produtos(){
     for (int i=0; i < produtos_cadastrados; i++)
         printf("listando produtos: %s\n", produtos[i]);
         
     }
-    
+
+// funçao responsavel por excluir um cliente ja cadastrado
     void excluir_cliente(){
      int i = 0;
         printf("escolha qual cliente deseja excluir: ");
         scanf("%d", &i);
 
+        strcpy(clientes[i],  "" );
+
+    
+
     }
     
     
-    
+// inicio da execuçao do codigo
 int main()
 {
     do{
@@ -60,38 +70,38 @@ int main()
     printf("4. Para listar clientes\n");
     printf("5. para listar os produtos. \n");
     printf("6. para excluir um cliente.\n");
-    scanf("%d", &esc);
+    scanf("%d", &escolha);
     
-    if (esc == 1){
+    if (escolha == 1){
         add_cliente();   
     }
-    else if(esc == 2){
+    else if(escolha == 2){
         add_produto();
     }
-    else if (esc == 3){
+    else if (escolha == 3){
         printf(" venda\n");
     }
     
     
-    else if (esc == 4){
+    else if (escolha == 4){
         listar_clientes();
     }
     
-    else if (esc == 5){
+    else if (escolha == 5){
         listar_produtos();
     }
     
-    else if (esc== 6){
+    else if (escolha == 6){
         excluir_cliente();
     }
     
-    else if (esc ==0){
+    else if (escolha ==0){
         printf(" saindo do programa\n");
     }
     
     
     
-    } while(esc > 0);
+    } while(escolha > 0);
     
     
     return 0;
